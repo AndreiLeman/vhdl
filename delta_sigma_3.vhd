fa96a1e08555abedfbf2c035c859d99c2d822f09
xaxaxa <xaxaxa@xa.us.to>
2016-04-30 14:18:49 +0100

    delta_sigma_3: improve timing and accuracy
    * make use of the full range of the integrator
    * timing optimizations (can now meet timing constraints at 300MHz clk on
    xc6slx9)

--- delta_sigma_3.vhd
+++ delta_sigma_3.vhd
@@ -6,8 +6,10 @@ use ieee.std_logic_1164.all;
 -- accumulation rate is specified in 1/cycle
 entity deltaSigmaModulator3 is
 	generic(--accumulation_rate: real := 0.00174216;
-				accumulation_rate_order: integer := 10;
-				threshold: real := 0.022);
+				accumulation_rate_order: integer := 7
+				--threshold: real := 0.022
+				--threshold: integer := 33554432*8-1
+				);
 	port(clk: in std_logic;
 			datain: in signed(31 downto 0);
 			dataout: out std_logic);
@@ -22,14 +24,15 @@ architecture a of deltaSigmaModulator3 is
 begin
 	--sum1 <= (datain(31)&datain(31 downto 1))+to_signed(1073741823,32);
 	--sum2 <= (datain(31)&datain(31 downto 1))-to_signed(1073741823,32);
-	tmp <= to_signed(1073741823,32) when cmpout='0' else to_signed(-1073741823,32);
-	tmpsum <= (datain(31)&datain(31 downto 1))+tmp;
+	tmp <= to_signed(1073741824,32) when cmpout='0' else to_signed(-1073741824,32);
+	tmpsum <= (datain(31)&datain(31 downto 1))+tmp when rising_edge(clk);
 	--sum_scaled <= tmpsum*to_signed(asdfg,16);
 	--sum_scaled <= tmpsum(31 downto accumulation_rate_order);
-	integral <= integral+tmpsum(31 downto accumulation_rate_order) when rising_edge(clk);
+	integral <= integral + ((accumulation_rate_order-1 downto 0=>tmpsum(31))
+									& tmpsum(31 downto accumulation_rate_order)) when rising_edge(clk);
 	lastout <= cmpout when rising_edge(clk);
-	cmpout <= '1' when lastout='0' and integral>integer(threshold*real(2)**31) else
-				'0' when lastout='1' and integral<integer(-threshold*real(2)**31) else
+	cmpout <= '1' when lastout='0' and integral(31 downto 30)="01" else
+				'0' when lastout='1' and integral(31 downto 30)="10" else
 				lastout;
 	
 	--cmpout <= not integral(31);

041676a5f50e1e2c6e2b6c1b89b81faaee4099dd
xaxaxa <xaxaxa@xa.us.to>
2016-06-23 23:58:36 +0100

    dsssDecoder3: support comb mode; move code generator out of the decoder

--- dsss_decoder_3.vhd
+++ dsss_decoder_3.vhd
@@ -5,12 +5,13 @@ use work.dcram;
 use work.sr_bit;
 use work.sr_unsigned;
 use work.sr_signed;
-use work.dsssCode1;
 entity dsssDecoder3 is
-	generic(clkdiv: integer := 225;
-			clkdiv_order: integer := 8;
+	generic(clkDiv: integer := 225;
+			clkDivOrder: integer := 8;
 			inbits: integer := 10;
-			outbits: integer := 17);
+			outbits: integer := 17;
+			codeLenOrder: integer := 10;
+			combSeparationOrder: integer := 0);
 	port(
 		-- divclk runs at Fs (din sample rate); coreclk runs at Fs * clkdiv
 		coreclk,divclk: in std_logic;
@@ -20,33 +21,36 @@ entity dsssDecoder3 is
 		din: signed(inbits-1 downto 0);
 		
 		--divclkPhase should be 0 right after the rising edge of divclk
-		divclkPhase: in unsigned(clkdiv_order-1 downto 0);
+		divclkPhase: in unsigned(clkDivOrder-1 downto 0);
 		
-		--output (synchronous to coreclk)
+		--code rom signals (for connection to external code rom)
+		--clock is coreclk; delay must be 2 clock cycles
+		codeAddr: out unsigned(codeLenOrder-1 downto 0);
+		code: in std_logic;
+		
+		--output (synchronous to divclk)
 		outValid: out std_logic;
-		outAddr: out unsigned(clkdiv_order-1 downto 0);
+		outAddr: out unsigned(clkDivOrder+combSeparationOrder-1 downto 0);
 		outData: out signed(outbits-1 downto 0));
 end entity;
 architecture a of dsssDecoder3 is
-	constant codeLen_order: integer := 10;
-	constant codeLen: integer := 2**codeLen_order;
+	constant codeLen: integer := 2**codeLenOrder;
+	constant combSeparation: integer := 2**combSeparationOrder;
 	
-	--divclk phase
-	signal divclkPhase1,divclkPhase2,divclkPhase3,divclkPhase5: unsigned(clkdiv_order-1 downto 0);
+	--accumulator index
+	signal divclkPhase1: unsigned(clkDivOrder-1 downto 0);
+	signal ramPos1,ramPos2,ramPos3,ramPos4,ramPos5: unsigned(clkDivOrder+combSeparationOrder-1 downto 0);
 	
 	--code phase
-	signal codeStartPhase,codeStartPhaseNext: unsigned(codeLen_order-1 downto 0);
-	signal codePhase,codePhaseNext: unsigned(codeLen_order-1 downto 0);
+	signal codeStartPhase,codeStartPhaseNext: unsigned(codeLenOrder+combSeparationOrder-1 downto 0);
+	signal codePhase,codePhaseNext: unsigned(codeLenOrder+combSeparationOrder-1 downto 0);
 	signal shouldOutput,resetContents,resetContents1,resetContents2: std_logic;
 	
 	--input preprocessing
 	signal dinU,dinPreprocessed,dinPreprocessed3: signed(inbits-1 downto 0);
 	
-	--code rom
-	signal code: std_logic;
-	
 	--accumulator ram
-	signal accWAddr,accRAddr: unsigned(clkdiv_order-1 downto 0);
+	signal accWAddr,accRAddr: unsigned(clkDivOrder+combSeparationOrder-1 downto 0);
 	signal accWData,accRData: signed(outbits-1 downto 0);
 	signal accWData_tmp,accRData_tmp: std_logic_vector(outbits-1 downto 0);
 	
@@ -56,16 +60,17 @@ architecture a of dsssDecoder3 is
 	signal accSum: signed(outbits-1 downto 0);
 	
 	--output registers
-	signal outValid0, outValidS1: std_logic;
-	signal outAddr0, outAddrS1: unsigned(clkdiv_order-1 downto 0);
-	signal outData0, outDataS1: signed(outbits-1 downto 0);
+	signal outValid0,outValid0Next, outValidS1: std_logic;
+	signal outAddr0,outAddr0Next, outAddrS1: unsigned(clkDivOrder+combSeparationOrder-1 downto 0);
+	signal outData0,outData0Next, outDataS1: signed(outbits-1 downto 0);
 begin
 	--preprocessing
 	dinU <= din when din>=0 else -din;
 	dinPreprocessed <= dinU when rising_edge(divclk);
 	
 	--ram
-	ram: entity dcram generic map(width=>outbits, depthOrder=>clkdiv_order,
+	ram: entity dcram generic map(width=>outbits,
+			depthOrder=>clkDivOrder+combSeparationOrder,
 			outputRegistered=>true)
 		port map(rdclk=>coreclk,wrclk=>coreclk,						--clocks
 			rden=>'1',rdaddr=>accRAddr,rddata=>accRData_tmp,		--read side
@@ -78,28 +83,36 @@ begin
 	codeStartPhase <= codeStartPhaseNext when rising_edge(divclk);
 	
 	--start codePhase at codeStartPhase, and increment every coreclk cycle
-	codePhaseNext <= codeStartPhase when divclkPhase=0 else codePhase+1;
+	codePhaseNext <= codeStartPhase when divclkPhase=0 else codePhase+combSeparation;
 	codePhase <= codePhaseNext when rising_edge(coreclk);
-	resetContents <= '1' when codePhase=0 else '0';
+	resetContents <= '1' when codePhase(codeLenOrder+combSeparationOrder-1 downto combSeparationOrder)=0 else '0';
 	
 	divclkPhase1 <= divclkPhase when rising_edge(coreclk);
-	--synchronized: divclkPhase1, codePhase
+	ramPos1 <= divclkPhase1 & (not codePhase(combSeparationOrder-1 downto 0));
+	--synchronized: divclkPhase1, codePhase, ramPos1
 
 		--read from code rom and accumulator ram
-		cg: entity dsssCode1 port map(coreclk,codePhase,code);	--2 cycles of delay
-		accRAddr <= divclkPhase1;
+		codeAddr <= codePhase(codeLenOrder+combSeparationOrder-1 downto combSeparationOrder);
+			--2 cycles of delay between codeAddr and code
+		accRAddr <= ramPos1;
+			--2 cycles of delay between accRAddr and accRData
 
 	resetContents1 <= resetContents when rising_edge(coreclk);
 	resetContents2 <= resetContents1 when rising_edge(coreclk);
-	divclkPhase2 <= divclkPhase1 when rising_edge(coreclk);
-	divclkPhase3 <= divclkPhase2 when rising_edge(coreclk);
+	ramPos2 <= ramPos1 when rising_edge(coreclk);
+	ramPos3 <= ramPos2 when rising_edge(coreclk);
 	sr_din: entity sr_signed generic map(bits=>inbits,len=>3) port map(coreclk,dinPreprocessed,dinPreprocessed3);
 	--synchronized: resetContents2, divclkPhase3, accRData, code, and dinPreprocessed3
 
 		--output previous ram data if we are the 0th code phase
-		outAddr0 <= divclkPhase3;
-		outData0 <= accRData;
-		outValid0 <= resetContents2;
+		outAddr0Next <= ramPos3 when resetContents2='1' else outAddr0;
+		outData0Next <= accRData when resetContents2='1' else outData0;
+		outValid0Next <= resetContents2 when ramPos3(clkDivOrder+combSeparationOrder-1 downto combSeparationOrder)=0
+			else '1' when resetContents2='1'
+			else outValid0;
+		outAddr0 <= outAddr0Next when rising_edge(coreclk);
+		outData0 <= outData0Next when rising_edge(coreclk);
+		outValid0 <= outValid0Next when rising_edge(coreclk);
 
 		--calculate code * input data
 		accOperandNext <= dinPreprocessed3 when code='1' else -dinPreprocessed3;
@@ -112,19 +125,22 @@ begin
 		--calculate sum
 		accSum <= accBase+accOperand when rising_edge(coreclk);
 
-	sr_divclkPhase: entity sr_unsigned generic map(bits=>clkdiv_order,len=>2) port map(coreclk,divclkPhase3,divclkPhase5);
-	--synchronized: accSum, divclkPhase5
+	ramPos4 <= ramPos3 when rising_edge(coreclk);
+	--synchronized: outAddr0, outData0, outValid0, ramPos4
+		
+		--latch the output once per divclk cycle (so we can resample it to divclk)
+		outAddrS1 <= outAddr0 when ramPos3(clkDivOrder+combSeparationOrder-1 downto combSeparationOrder)=0 and rising_edge(coreclk);
+		outDataS1 <= outData0 when ramPos3(clkDivOrder+combSeparationOrder-1 downto combSeparationOrder)=0 and rising_edge(coreclk);
+		outValidS1 <= outValid0 when ramPos3(clkDivOrder+combSeparationOrder-1 downto combSeparationOrder)=0 and rising_edge(coreclk);
+	
+	ramPos5 <= ramPos4 when rising_edge(coreclk);
+	--synchronized: accSum, ramPos5
 	
 		--write result back to accumulator ram
-		accWAddr <= divclkPhase5;
+		accWAddr <= ramPos5;
 		accWData <= accSum;
-	
-	
-	--latch the output data and address once per divclk cycle
-	--outAddrS1 <= outAddr0 when outValid0='1' and rising_edge(coreclk);
-	--outData0 <= outAddr0 when outValid0='1' and rising_edge(coreclk);
-	
-	outAddr <= outAddr0;
-	outData <= outData0;
-	outValid <= outValid0;
+
+	outAddr <= outAddrS1 when falling_edge(divclk);
+	outData <= outDataS1 when falling_edge(divclk);
+	outValid <= outValidS1 when falling_edge(divclk);
 end a;

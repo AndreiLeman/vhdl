9fef832301d28a69f87914b5e0fe5d9d5fd8e1c2
xaxaxa <xaxaxa@xa.us.to>
2018-01-18 13:12:35 +0000

    misc

--- fft/test.C
+++ fft/test.C
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <fftw3.h>
 #include <assert.h>
+#include <string.h>
 #include <vector>
 #include <iostream>
 #include "fft.H"
@@ -21,8 +22,8 @@ void doTests() {
 	int range = 100;
 	int errors = 0;
 	
-	vector<int> sizes = {8,32,4096*2,1024*1024};
-	vector<int> cnt = {200000,20000,200,1};
+	vector<int> sizes = {8,32,1024*8,1024*256,1024*1024*4};
+	vector<int> cnt = {200000,20000,200,20,1};
 	assert(sizeof(complexd) == sizeof(fftw_complex));
 	for(int ii=0;ii<(int)sizes.size();ii++) {
 		int sz = sizes[ii];
@@ -50,8 +51,9 @@ void doTests() {
 			
 			
 			// compute using custom fft
+			//memcpy(fft2.arr, inp2, sz*sizeof(complexd));
 			ull tsc2a = rdtscl();
-			fft2.rearrange(inp2);
+			fft2.rearrange(inp2, fft2.arr);
 			ull tsc2b = rdtscl();
 			fft2.combineAll();
 			ull tsc2c = rdtscl();
